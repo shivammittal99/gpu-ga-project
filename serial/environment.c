24e2a35236a2f3925f7206830efe8d1c3cebbbba
Shivam Mittal <shivammittal99@gmail.com>
2019-04-08 23:22:04 +0530

    Add genetic algorithm steps
    Signed-off-by: Shivam Mittal <shivammittal99@gmail.com>

--- serial/environment.c
+++ serial/environment.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include "neuralnet.c"
 
-float evaluate(float gene[]) {
-	float inputs[2] = {rand(), rand()};
+int evaluate(float gene[]) {
+	float inputs[2];
 
-	return forward(inputs, gene);
+	int score = 0;
+
+	do {
+		inputs[0] = rand();
+		inputs[1] = rand();
+		forward(inputs, gene);
+		score++;
+	} while(((float)rand()) / ((float)RAND_MAX) > 0.5);
+
+	return score;
 }
--- serial/serial.c
+++ serial/serial.c
@@ -4,29 +4,66 @@
 #include "environment.c"
 #include "genome.c"
 
+const int POPULATION_SIZE = 128;
+
 genome *organism;
-float *score;
+int *score, max_score;
 
-void createGenomes(int population_size) {
-	organism = (genome *) malloc(sizeof(genome) * population_size);
+float frand() {
+	return ((float)rand()) / ((float)RAND_MAX);
+}
 
-	for(int i = 0; i < population_size; i++) {
+void createGenomes() {
+	organism = (genome *) malloc(sizeof(genome) * POPULATION_SIZE);
+
+	for(int i = 0; i < POPULATION_SIZE; i++) {
 		for(int j = 0; j < GENOME_LENGTH; j++) {
-			organism[i].genes[j] = ((float)rand()) / ((float)RAND_MAX) * 0.2 - 0.1;
+			organism[i].genes[j] = frand() * 0.2 - 0.1;
 		}
 	}
 }
 
-void selection() {
+int selection(float selection_cutoff) {
+	int selected = 0;
+	genome *new_generation = (genome *) malloc(sizeof(genome) * POPULATION_SIZE);
 
+	for(int i = 0; i < POPULATION_SIZE; i++) {
+		if(frand() * score[i] / max_score > selection_cutoff) {
+			new_generation[selected] = organism[i];
+			selected++;
+		}
+	}
+
+	free(organism);
+	organism = new_generation;
+	return selected;
 }
 
-void crossover() {
+void crossover(int num_parents) {
+	int total = num_parents;
 
+	int parent[2];
+
+	while(total < POPULATION_SIZE) {
+		parent[0] = rand() % num_parents;
+		parent[1] = rand() % num_parents;
+
+		for(int i = 0; i < GENOME_LENGTH; i++) {
+			organism[total].genes[i] = organism[parent[rand() % 2]].genes[i];
+		}
+
+		total++;
+	}
 }
 
-void mutate() {
-	
+void mutate(float mutation_rate) {
+	for(int i = 0; i < POPULATION_SIZE; i++) {
+		for(int j = 0; j < GENOME_LENGTH; j++) {
+			if(frand() < mutation_rate) {
+				organism[i].genes[j] = frand() * 0.2 - 0.1;
+			}
+		}
+	}
 }
 
 int main() {
@@ -34,29 +71,38 @@ int main() {
 
 	printf("Genome length: %d\n", GENOME_LENGTH);
 
-	const int POPULATION_SIZE = 128;
-
-	createGenomes(POPULATION_SIZE);
+	createGenomes();
 
 	printf("Generation size: %d\n", POPULATION_SIZE);
 
 	const int NUM_GENERATIONS = 20;
 
-	score = (float *) malloc(sizeof(float) * POPULATION_SIZE);
+	score = (int *) malloc(sizeof(int) * POPULATION_SIZE);
 
-	float max_score = 0;
+	max_score = 0;
+
+	FILE *fout = fopen("genomes.txt", "w");
+
+	fprintf(fout, "NUM_GENERATIONS = %d\n", NUM_GENERATIONS);
+	fprintf(fout, "POPULATION_SIZE = %d\n", POPULATION_SIZE);
+	fprintf(fout, "GENOME_LENGTH = %d\n", GENOME_LENGTH);
 
 	for(int i = 0; i < NUM_GENERATIONS; i++) {
 		for(int j = 0; j < POPULATION_SIZE; j++) {
-			score[j] = evaluate(organism[i].genes);
-			max_score = fmax(max_score, score[j]);
-			selection();
-			crossover();
-			mutate();
+			for(int k = 0; k < GENOME_LENGTH; k++) {
+				fprintf(fout, "%f ", organism[j].genes[k]);
+			}
+			fprintf(fout, "\n");
+			score[j] = evaluate(organism[j].genes);
+			max_score = max_score > score[j] ? max_score : score[j];
 		}
+		int selected = selection(0.15);
+		crossover(selected);
+		mutate(1e-3);
 	}
 
-	printf("max score: %f\n", max_score);
+	printf("max score: %d\n", max_score);
 
+	free(organism);
 	return 0;
 }
