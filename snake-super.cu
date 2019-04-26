2c9c14744193ca2e00d90c87555b81eb89a8a31a
harshini9898 <harshini9898@gmail.com>
2019-04-26 11:05:52 +0530

    updated the postion stream segment of code

--- snake-super.cu
+++ snake-super.cu
@@ -495,6 +495,14 @@ int main() {
 
 		evaluate<<<blocks, threads, sizeof(float) * GENOME_LENGTH>>>(d_organism, d_foods, d_fitness_score, GENOME_LENGTH);
 
+		curandSetStream(prng,stream1);
+		curandGenerate(prng, random_uints[0], 2 * POPULATION_SIZE);
+		curandGenerate(prng, random_uints[1], POPULATION_SIZE);	
+		curandGenerateUniform(prng, random_floats[0], POPULATION_SIZE * GENOME_LENGTH);
+		curandGenerateNormal(prng, random_floats[1], POPULATION_SIZE * GENOME_LENGTH, 0.0, 1.0);
+		thrust::sequence(thrust::cuda::par.on(stream1), thrust_indices_ptr, thrust_indices_ptr + POPULATION_SIZE);
+		cudaStreamSynchronize(stream1);	
+
 		cudaMemcpy(fitness_score, d_fitness_score, sizeof(int) * POPULATION_SIZE, cudaMemcpyDeviceToHost);
 		cudaErrorTrace();
 		
@@ -520,13 +528,7 @@ int main() {
 
 		const int selected = 0.15 * POPULATION_SIZE;
 
-		curandSetStream(prng,stream1);
-		curandGenerate(prng, random_uints[0], 2 * POPULATION_SIZE);
-		curandGenerate(prng, random_uints[1], POPULATION_SIZE);	
-		curandGenerateUniform(prng, random_floats[0], POPULATION_SIZE * GENOME_LENGTH);
-		curandGenerateNormal(prng, random_floats[1], POPULATION_SIZE * GENOME_LENGTH, 0.0, 1.0);
-		thrust::sequence(thrust::cuda::par.on(stream1), thrust_indices_ptr, thrust_indices_ptr + POPULATION_SIZE);
-		cudaStreamSynchronize(stream1);	
+		
 
 		//thrust::sequence(thrust_indices_ptr, thrust_indices_ptr + POPULATION_SIZE);
 		thrust::sort_by_key(thrust_fitness_score_ptr, thrust_fitness_score_ptr + POPULATION_SIZE, thrust_indices_ptr, thrust::greater<int>());
